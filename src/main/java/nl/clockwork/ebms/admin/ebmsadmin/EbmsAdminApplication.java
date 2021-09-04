ce9bd4e1cb63c9aa32c2bfa55d4ce4fca40bd1ef
eluinstra <eluinstra@luin.dev>
2021-09-04 23:06:11 +0200

    added spring-boot + vaadin

---
+++ src/main/java/nl/clockwork/ebms/admin/ebmsadmin/EbmsAdminApplication.java
@@ -0,0 +1,14 @@
+package nl.clockwork.ebms.admin.ebmsadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbmsAdminApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(EbmsAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/clockwork/ebms/admin/ebmsadmin/EbmsAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.clockwork.ebms.admin.ebmsadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbmsAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
