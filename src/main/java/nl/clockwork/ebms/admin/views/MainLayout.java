ccd241c5af118a7482160894ccc7cf823db2d430
eluinstra <eluinstra@luin.dev>
2021-09-05 23:26:01 +0200

    added material theme

--- src/main/java/nl/clockwork/ebms/admin/views/MainLayout.java
+++ src/main/java/nl/clockwork/ebms/admin/views/MainLayout.java
@@ -1,7 +1,11 @@
 package nl.clockwork.ebms.admin.views;
 
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.contextmenu.MenuItem;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Hr;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.menubar.MenuBar;
@@ -9,11 +13,13 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.material.Material;
 
 import lombok.val;
 
 @PWA(name = "ebms-admin-vaadin", shortName = "ebms-admin-vaadin", enableInstallPrompt = false)
-@Theme(themeFolder = "ebms-admin-vaadin")
+@Theme(value = Material.class)
+@CssImport("./styles/custom-styles.css")
 @PageTitle("Main")
 public class MainLayout extends AppLayout
 {
@@ -26,28 +32,36 @@ public class MainLayout extends AppLayout
 	private Image createImage()
 	{
 		val image = new Image("images/ebms_admin.gif",getTranslation("home"));
-		image.addClickListener(e -> image.getUI().ifPresent(ui -> ui.navigate(HomeView.class)));
+		image.addClickListener(e -> UI.getCurrent().navigate(HomeView.class));
 		return image;
 	}
 
 	private MenuBar createMenuBar()
 	{
 		val menuBar = new MenuBar();
-		menuBar.addItem(new RouterLink(getTranslation("home"),HomeView.class));
+		menuBar.setOpenOnHover(true);
+		menuBar.addItem(createRouterLink(getTranslation("home"),HomeView.class));
 		createCPAServiceMenu(menuBar.addItem(getTranslation("cpaService")));
 		createMessageServiceMenu(menuBar.addItem(getTranslation("messageService")));
-		createAdvancedMenu(menuBar.addItem(getTranslation("advanced")));
-		menuBar.addItem(new RouterLink(getTranslation("about"),AboutView.class));
+		createAdvancedMenu(menuBar.addItem(new Button(getTranslation("advanced"))));
+		menuBar.addItem(createRouterLink(getTranslation("about"),AboutView.class));
 		return menuBar;
+	}
+
+	private RouterLink createRouterLink(String text, Class<? extends Component> component)
+	{
+		val result = new RouterLink(text,component);
+		result.getElement().getStyle().set("text-decoration","none");
+		return result;
 	}
 
 	private void createCPAServiceMenu(MenuItem cpaService)
 	{
 		val subMenu = cpaService.getSubMenu();
 		subMenu.addItem(getTranslation("cpas"));
-		subMenu.addItem(new Hr());
+		subMenu.add(new Hr());
 		subMenu.addItem(getTranslation("urlMappings"));
-		subMenu.addItem(new Hr());
+		subMenu.add(new Hr());
 		subMenu.addItem(getTranslation("certificateMappings"));
 	}
 
@@ -55,14 +69,14 @@ public class MainLayout extends AppLayout
 	{
 		val subMenu = messageService.getSubMenu();
 		subMenu.addItem(getTranslation("ping"));
-		subMenu.addItem(new Hr());
+		subMenu.add(new Hr());
 		subMenu.addItem(getTranslation("unprocessedMessages"));
-		subMenu.addItem(new Hr());
+		subMenu.add(new Hr());
 		subMenu.addItem(getTranslation("messageEvents"));
-		subMenu.addItem(new Hr());
+		subMenu.add(new Hr());
 		subMenu.addItem(getTranslation("messageSend"));
 		subMenu.addItem(getTranslation("messageResend"));
-		subMenu.addItem(new Hr());
+		subMenu.add(new Hr());
 		subMenu.addItem(getTranslation("messageStatus"));
 	}
 
@@ -71,7 +85,7 @@ public class MainLayout extends AppLayout
 		val subMenu = advanced.getSubMenu();
 		subMenu.addItem(getTranslation("traffic"));
 		subMenu.addItem(getTranslation("trafficChart"));
-		subMenu.addItem(new Hr());
+		subMenu.add(new Hr());
 		subMenu.addItem(getTranslation("cpas"));
 		subMenu.addItem(getTranslation("messages"));
 	}
