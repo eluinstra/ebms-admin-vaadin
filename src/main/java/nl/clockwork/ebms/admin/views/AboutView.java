9f7cce02a196409ae638f5db7db7f11815a3f1ae
eluinstra <eluinstra@luin.dev>
2021-09-14 20:47:00 +0200

    replaced Card by Accordion

--- src/main/java/nl/clockwork/ebms/admin/views/AboutView.java
+++ src/main/java/nl/clockwork/ebms/admin/views/AboutView.java
@@ -15,12 +15,13 @@
  */
 package nl.clockwork.ebms.admin.views;
 
-import com.github.appreciated.card.Card;
-import com.github.appreciated.card.content.Item;
-import com.github.appreciated.card.label.TitleLabel;
-import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.HtmlComponent;
+import com.vaadin.flow.component.accordion.Accordion;
+import com.vaadin.flow.component.accordion.AccordionPanel;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Pre;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -35,44 +36,37 @@ public class AboutView extends VerticalLayout
 	public AboutView()
 	{
 		add(new H1(getTranslation("about")));
-		add(createVersionsCard());
-		add(createPropertiesCard());
-		add(createLicenseInfo());
+		val accordion = new Accordion();
+		accordion.setSizeFull();
+		accordion.add(createVersionsCard());
+		accordion.add(createPropertiesCard());
+		accordion.add(createLicenseInfo());
+		add(accordion);
 	}
 
-	private Card createVersionsCard()
+	private AccordionPanel createVersionsCard()
 	{
-		val result = new Card(
-			// new TitleLabel(getTranslation("versions")),
-			new Item("ebms-admin",Utils.readVersion("/META-INF/maven/nl.clockwork.ebms.admin/ebms-admin/pom.properties")),
-			new Item("ebms-core",Utils.readVersion("/META-INF/maven/nl.clockwork.ebms/ebms-core/pom.properties"))
-		);
-		result.setWidthFull();
-		return result;
+		val properties = new Div();
+		properties.add(new Span(Utils.readVersion("/META-INF/maven/nl.clockwork.ebms.admin/ebms-admin/pom.properties")));
+		properties.add(new HtmlComponent("br"));
+		properties.add(new Span(Utils.readVersion("/META-INF/maven/nl.clockwork.ebms/ebms-core/pom.properties")));
+		return new AccordionPanel(getTranslation("versions"),properties);
 	}
 
-	private Component createPropertiesCard()
+	private AccordionPanel createPropertiesCard()
 	{
-		val properties = new Item(getTranslation("properties"),"prop1=val1\nprop2=val2\nprop3=val3");
-		properties.getElement().getStyle().set("white-space","pre-wrap");
-		val result = new Card(
-			// new TitleLabel(getTranslation("properties")),
-			properties
+		return new AccordionPanel(
+			getTranslation("properties"),
+			new Pre("prop1=val1\nprop2=val2\nprop3=val3")
 		);
-		result.setWidthFull();
-		return result;
 	}
 
-	private Component createLicenseInfo()
+	private AccordionPanel createLicenseInfo()
 	{
-		val license = new Item(getTranslation("license"),"Copyright 2021 Ordina\n\nLicensed under the Apache License, Version 2.0 (the \"License\");\nyou may not use this file except in compliance with the License.\nYou may obtain a copy of the License at\n\n  http://www.apache.org/licenses/LICENSE-2.0\n\nUnless required by applicable law or agreed to in writing, software\ndistributed under the License is distributed on an \"AS IS\" BASIS,\nWITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\nSee the License for the specific language governing permissions and\nlimitations under the License.\n");
-		license.getElement().getStyle().set("white-space","pre-wrap");
-		val result = new Card(
-			// new TitleLabel(getTranslation("license")),
-			license
+		return new AccordionPanel(
+			getTranslation("license"),
+			new Pre("Copyright 2021 Ordina\n\nLicensed under the Apache License, Version 2.0 (the \"License\");\nyou may not use this file except in compliance with the License.\nYou may obtain a copy of the License at\n\n  http://www.apache.org/licenses/LICENSE-2.0\n\nUnless required by applicable law or agreed to in writing, software\ndistributed under the License is distributed on an \"AS IS\" BASIS,\nWITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\nSee the License for the specific language governing permissions and\nlimitations under the License.\n")
 		);
-		result.setWidthFull();
-		return result;
 	}
 
 }
